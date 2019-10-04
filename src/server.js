1bb45b87f9191c4ce3f635efe8306256b57461a8
Tarcisio Henrique de Moraes Hazin <titohazin@gmail.com>
2019-10-04 18:26:29 -0300

    complemento de recursos

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 const routes = require('./routes');
 
 // Utlizar o framework express
@@ -12,6 +14,10 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-evn0g' +
     useUnifiedTopology: true
 });
 
+// Criar uma rota para resgatar arquivos que foram uploadados na aplicação:
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+// Aceitar todas as origens de requisições
+app.use(cors({origin: 'http://localhost:3000'}))
 // Interpretar req JSON no body
 app.use(express.json());
 app.use(routes);
